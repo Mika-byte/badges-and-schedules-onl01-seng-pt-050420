b2f94ba5a002febd2f12fe4ba4183f4742b97411
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-14 02:21:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,24 +1,25 @@
-def badge_maker(name) 
-"Hello, my name is #{name}." 
-end 
-
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
 def batch_badge_creator(name)
-name.each do |badge_maker| 
-badge_maker(name)
-  end
-end  
-
-
-def assigns_rooms(name)
-  room = 0
   name.collect do |name|
-   room += 1
-    "Hello, #{name}! You'll be assigned to #{room}!"
-  end 
+    badge_maker(name)
+  end
+end
+def assign_rooms(name)
+    room = 0
+  name.collect do |name|
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+def printer(name)
+  batch_badge_creator(name).collect do |value|
+    puts value
+  end
+  assign_rooms(name).collect do |value|
+    puts value
+  end
 end
 
-def printer(name)
-   batch_badge_creator(name).collect do |value|
-     puts value
-  end
-end  
+ 
