0dd5a40dc82ce567002f1ddc2647e2688ed0b57a
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-14 02:07:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def badge_maker(name) 
+"Hello, my name is #{name}." 
+end 
+
+def batch_badge_creator(a
+badge_maker.each do |badge_maker| 
+puts "Hello, my name is #{badge_maker}." 
+end
