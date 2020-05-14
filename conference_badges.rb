de6074d083f6329e1c39b5ce4a8693c033b11fb4
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-14 02:08:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,13 @@ def batch_badge_creator(a
 badge_maker.each do |badge_maker| 
 puts "Hello, my name is #{badge_maker}." 
 end
+def assigns_rooms(name)
+  room = 0
+  name.collect do |name|
+   room += 1
+    "Hello, #{name}! You'll be assigned to #{room}!"
+  end 
+end
+
+def printer(name)
+  puts batch_badge_creator(name).collect do
